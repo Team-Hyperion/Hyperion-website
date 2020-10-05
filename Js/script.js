f9539b8f043670d2f221079e6a3badaaca65372d
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-10-05 13:20:42 -0400

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,1 @@
-var fs = require('fs');
-
-function information() {
-    var date_input = document.getElementById("Date");
-    var feedback_input = document.getElementById("Feedback");
-
-    document.write(date_input + feedback_input)
-}
-
-
-fs.writeFileSync('test.txt', date_input )
-fs.writeFileSync('test.txt', feedback_input )
-console.log("test");
+console.log("test")
