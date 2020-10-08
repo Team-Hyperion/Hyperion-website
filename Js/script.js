b21fc9308b446c86f9a4ce15e2f136772373162d
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-10-08 11:43:45 -0400

    added dark mode

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,29 @@
-console.log("test")
+let dark_mode = localStorage.getItem('dark_mode');
+const dark_mode_toggle = document.querySelector("#theme_changer");
+
+const enable_dark_mode = () =>{
+    document.body.classList.add('dark_theme')
+    localStorage.setItem('dark_mode', 'enabled')
+}
+
+const disable_dark_mode = () =>{
+    document.body.classList.remove('dark_theme')
+    localStorage.setItem('dark_mode', null)
+    
+}
+
+if (dark_mode === 'enabled'){
+    enable_dark_mode();
+}
+
+dark_mode_toggle.addEventListener("click", () => {
+    dark_mode = localStorage.getItem('dark_mode');
+    if (dark_mode !== 'enabled'){
+        enable_dark_mode();
+        console.log(dark_mode)
+    }else{
+        disable_dark_mode();
+        console.log(dark_mode)
+    }
+})
+
