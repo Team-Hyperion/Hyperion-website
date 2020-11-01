fa0f25cedbf9007bf3a2213da21dbaf9e1fb1f78
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-10-31 23:09:07 -0400

    Update imageLoader.js

--- Js/imageLoader.js
+++ Js/imageLoader.js
@@ -1,3 +1,6 @@
+/**
+when the file/function is called, a picture will be added to each div
+*/
 $(document).ready( function() {
     $("div").each( function(i) {
         $(this).append("<img src='Media/"+(++i)+".png' width='600' height='600' />");
