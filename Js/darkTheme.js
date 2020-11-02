8cbf5e8b7453a354585f3d928a495da9268dbf5e
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-11-02 14:41:20 -0500

    Update darkTheme.js

--- Js/darkTheme.js
+++ Js/darkTheme.js
@@ -1,6 +1,10 @@
+// code modified from https://youtu.be/wodWDIdV9BY
+
+//gets info from other html files and local storage
 let dark_mode = localStorage.getItem('dark_mode');
 const dark_mode_toggle = document.querySelector("#theme_changer");
 
+//changes the theme when called
 const enable_dark_mode = () =>{
     document.body.classList.add('dark_theme')
     localStorage.setItem('dark_mode', 'enabled')
@@ -12,10 +16,12 @@ const disable_dark_mode = () =>{
     
 }
 
+//edge case
 if (dark_mode === 'enabled'){
     enable_dark_mode();
 }
 
+//when button is pressed calls another function
 dark_mode_toggle.addEventListener("click", () => {
     dark_mode = localStorage.getItem('dark_mode');
     if (dark_mode !== 'enabled'){
@@ -26,4 +32,3 @@ dark_mode_toggle.addEventListener("click", () => {
         console.log(dark_mode)
     }
 })
-
