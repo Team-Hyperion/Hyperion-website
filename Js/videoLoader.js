0fe379d98a49424f3e287a61719879d150191e0a
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-11-04 11:16:05 -0500

    Update videoLoader.js

--- Js/videoLoader.js
+++ Js/videoLoader.js
@@ -2,10 +2,16 @@
  *adds video i for each div there are in the html file
  */
 $(document).ready( function() {
+    $("li2").each( function() {
+        for(var i = 1; i<= 9; i++){
+            $('#target').append($("<video class='column' width='630' height='600' controls >"))
+          }
+    });
     $("video").each( function(i) {
-        $(this).append("<source src='Media/Videos/"+(++i)+".mp4'/>");
+        $(this).append("<source src='Media/"+(++i)+".mp4'/>");
     });
 });
+
 
 /**
  *reverses the order of the items in li2
