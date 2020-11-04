84e43bf6204c4ff28d23ed6b4c8455a9a0256c7e
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-11-04 11:15:35 -0500

    Update imageLoader.js

--- Js/imageLoader.js
+++ Js/imageLoader.js
@@ -1,9 +1,17 @@
-/**
- adds image i for each div there are in the html file
- */
 $(document).ready( function() {
+		/**
+ 		*creates div tags depending on number
+ 		*/
+    $("li2").each( function() {
+        for(var i = 1; i<= 9; i++){
+            $('#target').append($("<div class='column'>"))
+          }
+    });
+		/**
+ 		adds image i for each div there are in the html file
+ 		*/
     $("div").each( function(i) {
-        $(this).append("<img src='Media/Photos/"+(++i)+".png' width='630' height='600'/>");
+        $(this).append("<img src='Media/"+(++i)+".png' width='630' height='600'/>");
     });
 });
 
