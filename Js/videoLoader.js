bd7cf31b469e20d4431bc8bb93a4d5a8f70d55ba
Ryan Pham <71665776+spin64@users.noreply.github.com>
2020-11-05 13:03:24 -0500

    Update videoLoader.js

--- Js/videoLoader.js
+++ Js/videoLoader.js
@@ -1,12 +1,18 @@
 /**
- *adds video i for each div there are in the html file
- */
+*function that calls other functions on website launch
+*/
 $(document).ready( function() {
+    /**
+    *creates x amount of divs
+    */
     $("li2").each( function() {
         for(var i = 1; i<= 9; i++){
             $('#target').append($("<video class='column' width='630' height='600' controls >"))
           }
     });
+    /**
+    *adds video i for each div there are in the html file
+    */
     $("video").each( function(i) {
         $(this).append("<source src='Media/"+(++i)+".mp4'/>");
     });
